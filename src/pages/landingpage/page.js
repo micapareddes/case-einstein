eff2987799e7fec78ae2588184284983f645d3a7
micapareddes <micapareddes@gmail.com>
2024-09-08 20:40:06 -0300

    Adiciona componente de impacto banner a landingpage

--- src/pages/landingpage/page.js
+++ src/pages/landingpage/page.js
@@ -1,8 +1,10 @@
 import { Button } from "../../components/button.js"
 import { Header } from "../../components/header.js"
+import { ImpactoBanner } from "../../components/impacto-banner.js"
 
 export function LandingPage() {
     const body = document.getElementById('body')
+    const about = document.getElementById('about')
     const buttonsLaptop = document.getElementById('container-action-buttons-laptop')
     const buttonsTabletAndMobile = document.getElementById('container-action-buttons-mobile-and-tablet')
 
@@ -25,4 +27,8 @@ export function LandingPage() {
             title: 'Seja um aluno',
         })
     )
+    about.prepend(
+        ImpactoBanner()
+    )
+
 } LandingPage()
